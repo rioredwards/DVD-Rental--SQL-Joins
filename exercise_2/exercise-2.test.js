fcd4af622f8f9595187e63de8b325480bafd3bdc
Rio Edwards <rioredwards@gmail.com>
2022-11-08 15:28:20 -0800

    Pass Test for Exercise-2

--- exercise_2/exercise-2.test.js
+++ exercise_2/exercise-2.test.js
@@ -4,7 +4,7 @@ const pool = require('../lib/utils/pool');
 describe('exercise 2', () => {
   afterAll(() => pool.end());
 
-  it.skip('find all rental dates for customers with the first_name Patricia', async () => {
+  it('find all rental dates for customers with the first_name Patricia', async () => {
     const { rows } = await pool.query(
       fs.readFileSync(`${__dirname}/exercise-2.sql`, 'utf-8')
     );
