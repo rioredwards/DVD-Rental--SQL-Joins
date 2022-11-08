dd4a652b1070bd246a1810bb8b40f9d11ae2bd7d
Rio Edwards <rioredwards@gmail.com>
2022-11-08 15:42:35 -0800

    Pass Test for Exercise-3

--- exercise_3/exercise-3.sql
+++ exercise_3/exercise-3.sql
@@ -1,3 +1,10 @@
 -- find all film titles with the category Action
 -- use a LEFT JOIN FROM film onto film_category
 -- use LEFT JOIN FROM film_categor onto category
+select film.title
+from 
+	film
+    left join film_category on film.film_id = film_category.film_id
+	left join category on film_category.category_id = category.category_id
+where
+	category.name = 'Action';
