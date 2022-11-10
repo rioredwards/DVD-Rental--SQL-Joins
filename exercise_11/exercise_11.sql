96cacd84b96c51e0a249e3bc87bf5c2aa0c520f0
Rio Edwards <rioredwards@gmail.com>
2022-11-09 16:21:04 -0800

    Pass Test for Exercise-11

--- exercise_11/exercise_11.sql
+++ exercise_11/exercise_11.sql
@@ -3,3 +3,14 @@
 -- GROUP BY the film_id
 -- ORDER in descending order
 -- LIMIT 10
+
+SELECT 
+    film.title, COUNT(rental.inventory_id)
+FROM
+    film
+    LEFT JOIN inventory ON film.film_id = inventory.film_id
+    LEFT JOIN rental ON inventory.inventory_id = rental.inventory_id
+GROUP BY
+    film.film_id
+ORDER BY COUNT(rental.inventory_id) DESC
+LIMIT 10
